824c438fc5018de3a593f18386f14ff5c044a264
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2021-12-17 17:15:17 +0100

    Checked out different parameters in `glDrawElements` Also tested polygon mode.

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,9 @@ int main() {
             0, 1, 3,  // first Triangle
             1, 2, 3   // second Triangle
     };
-    unsigned int VBO, VAO, EBO;
+
+    // Vertex Array Object, Vertex Buffer Object, Element Buffer Object
+    unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
@@ -98,9 +100,20 @@ int main() {
 
         shader.use();
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // Draw in Polygon mode
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+        // Draw both triangles
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        // Draw first triangle
+        //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+        // Draw second triangle
+        //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT,  (void*)(3 * sizeof(GL_UNSIGNED_INT)));
         // glBindVertexArray(0); // no need to unbind it every time
+
+        // Reste to default fill mode
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
